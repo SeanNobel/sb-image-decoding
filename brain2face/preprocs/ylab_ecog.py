a059f0c1676e611b6b97190de7e8ab0ea20a94ee
SeanNobel <sensho19950503@gmail.com>
2023-06-22 18:37:26 +0900

    prev: ECoG was downsampled to 30Hz while loading -> now: load as it is 2000Hz

--- brain2face/preprocs/ylab_ecog.py
+++ brain2face/preprocs/ylab_ecog.py
@@ -22,28 +22,35 @@ import torch
 
 torch.multiprocessing.set_start_method("spawn", force=True)
 
-from brain2face.utils.brain_preproc import scale_and_clamp, baseline_correction
+from brain2face.utils.brain_preproc import brain_preproc
+from brain2face.utils.preproc_utils import crop_longer
 
 
 def load_ecog_data(args, sync_df: pd.DataFrame) -> np.ndarray:
     """
     Returns:
-        ecog_raw: ( channels, timesteps )
+        ecog_raw: ( channels, timesteps@2000Hz )
     """
-    brainwave_name = sync_df.brainwave_name.values[0]
-    dat = h5py.File(args.ecog_data_root + brainwave_name, "r")["ecog_dat"][()]
+    bw_name = sync_df.brainwave_name.values[0]
+    dat = h5py.File(args.ecog_data_root + bw_name, "r")["ecog_dat"][()]
 
-    brainwave_frames = sync_df.brainwave_frame.values.astype(int)
+    bw_frames = sync_df.brainwave_frame.values.astype(int)
     ecog_data = []
-    for i, frame in enumerate(tqdm(brainwave_frames)):
-        if sync_df.brainwave_name.values[i] != brainwave_name:
+    for i in tqdm(range(len(bw_frames))):
+        if sync_df.brainwave_name.values[i] != bw_name:
             cprint("Loading new ECoG file.", "yellow")
-            brainwave_name = sync_df.brainwave_name.values[i]
-            dat = h5py.File(args.ecog_data_root + brainwave_name, "r")["ecog_dat"][()]
+            bw_name = sync_df.brainwave_name.values[i]
+            dat = h5py.File(args.ecog_data_root + bw_name, "r")["ecog_dat"][()]
 
-        ecog_data.append(dat[frame])
+        # ecog_data.append(dat[frame])
+        # NOTE: For the final frame, takes number of frames that corresponds to downsampling
+        # rate caused by linear interpolation between videos and ECoG
+        if i == len(bw_frames) - 1:
+            ecog_data.append(dat[bw_frames[i] : bw_frames[i] + args.brain_orig_sfreq // args.fps])
+        else:
+            ecog_data.append(dat[bw_frames[i] : bw_frames[i+1]])
 
-    return np.stack(ecog_data).T
+    return np.concatenate(ecog_data).T.astype(np.float64)
 
 
 def face_preproc(face_path: str, sync_df: pd.DataFrame, segment_len: int) -> np.ndarray:
@@ -93,8 +100,6 @@ def main(args: DictConfig) -> None:
     session_ids = [int(re.split("[._]", path)[-2]) for path in face_paths]
     cprint(f"> {len(session_ids)} sessions: {session_ids}", "cyan")
 
-    segment_len = args.seq_len * args.fps
-
     for i, (session_id, face_path) in enumerate(zip(session_ids, face_paths)):
         cprint(f">> Processing subject number {i}", color="cyan")
 
@@ -109,19 +114,25 @@ def main(args: DictConfig) -> None:
         if sync_df.shape[0] > 0:
             cprint(f">> Sync data: {sync_df.shape}", "cyan")
 
-            ecog_raw = load_ecog_data(args, sync_df)
-            print(ecog_raw.shape)
-            sys.exit()
+            ecog_raw = load_ecog_data(args, sync_df) # ( channels, timesteps )
 
-            X = ecog_preproc(args, ecog_raw, segment_len)
+            X = brain_preproc(
+                args,
+                brain=ecog_raw,
+                segment_len=int(args.brain_resample_sfreq * args.seq_len)
+            )
             cprint(f"Subject {i} ECoG: {X.shape}", "cyan")
 
-            Y = face_preproc(face_path, sync_df, segment_len)
+            Y = face_preproc(
+                face_path,
+                sync_df,
+                segment_len=int(args.fps * args.seq_len)
+            )
             cprint(f"Subject {i} face: {Y.shape}", "cyan")
 
-            assert len(X) == len(Y)
+            X, Y = crop_longer(X, Y)
 
-            data_dir = f"{args.root_dir}/data/YLab/{args.preproc_name}/S{i}/"
+            data_dir = f"{args.root_dir}/data/preprocessed/ylab/{args.preproc_name}/S{i}/"
             os.makedirs(data_dir, exist_ok=True)
             np.save(data_dir + "brain.npy", X)
             np.save(data_dir + "face.npy", Y)
