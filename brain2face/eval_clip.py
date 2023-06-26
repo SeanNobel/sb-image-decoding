7ffaeb7f02c6a0b6f14a7035882ef665a04dbd25
SeanNobel <sensho19950503@gmail.com>
2023-06-26 15:53:10 +0900

    first CLIP prior training done

--- brain2face/eval_clip.py
+++ brain2face/eval_clip.py
@@ -20,13 +20,17 @@ from brain2face.utils.layout import ch_locations_2d
 
 
 @torch.no_grad()
-def infer(args: DictConfig):
+def infer(args: DictConfig) -> None:
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
     run_name = "".join([k + "-" + str(v) + "_" for k, v in sorted(args.eval.items())])
     run_dir = os.path.join("runs", args.dataset.lower(), run_name)
     assert os.path.exists(run_dir), "run_dir doesn't exist."
+
+    save_dir = os.path.join("data/clip_embds", args.dataset.lower())
+    os.makedirs(os.path.join(save_dir, "face"), exist_ok=True)
+    os.makedirs(os.path.join(save_dir, "brain"), exist_ok=True)
 
     device = f"cuda:{args.cuda_id}"
 
@@ -102,7 +106,8 @@ def infer(args: DictConfig):
     # -----------------------
     #       Evaluation
     # -----------------------
-    i = 0
+    Z_list = []
+    Y_list = []
     for X, Y, subject_idxs in tqdm(train_loader):
         X, Y = X.to(device), Y.to(device)
 
@@ -110,19 +115,18 @@ def infer(args: DictConfig):
 
         if face_encoder is not None:
             Y = face_encoder(Y)
+            
+        Z_list.append(Z.cpu().numpy())
+        Y_list.append(Y.cpu().numpy())
 
-        for z, y in zip(Z, Y):
-            np.save(
-                os.path.join("data/clip_embds/uhd", f"{str(i).zfill(5)}.npy"),
-                z.cpu().numpy(),
-            )
-            np.save(
-                os.path.join("data/clip_embds/uhd", f"{str(i).zfill(5)}.npy"),
-                y.cpu().numpy(),
-            )
-
-            i += 1
-
+    np.save(
+        os.path.join(save_dir, "brain", "brain_embds.npy"),
+        np.concatenate(Z_list),
+    )
+    np.save(
+        os.path.join(save_dir, "face", "face_embds.npy"),
+        np.concatenate(Y_list),
+    )
 
 @hydra.main(version_base=None, config_path="../configs", config_name="default")
 def run(_args: DictConfig) -> None:
