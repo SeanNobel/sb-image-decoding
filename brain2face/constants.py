91fc05b4e0786bc7418ba1bf68ef53a518d9d0c8
SeanNobel <sensho19950503@gmail.com>
2023-06-27 13:11:06 +0900

    add EMB_CHUNK_SIZE for saving embeddings

--- brain2face/constants.py
+++ brain2face/constants.py
@@ -5,3 +5,5 @@ DLIB_PREDICTOR_PATH = ROOT + "/encoder4editing/shape_predictor_68_face_landmarks
 EXTRACTED_VIDEO_ROOT = ROOT + "/data/video"
 
 MONTAGE_INFO_PATH = ROOT + "/brain2face/utils/gTecUtils/settings/montage_EEGonly_32ch.xml"
+
+EMB_CHUNK_SIZE = 10000
