0c08d1aa7607344649381caf7c911966cc121561
SeanNobel <sensho19950503@gmail.com>
2023-08-03 21:32:11 +0900

    add logger

--- brain2face/train_video_decoder.py
+++ brain2face/train_video_decoder.py
@@ -6,14 +6,22 @@ from time import time
 from tqdm import tqdm
 from termcolor import cprint
 from typing import Union, Optional
+import logging
 import wandb
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-from dalle2_video.dalle2_video import UnetTemporalConv, VideoDecoder
-from dalle2_video.trainer import VideoDecoderTrainer
+from dalle2_video.dalle2_video import (
+    Unet3D,
+    UnetTemporalConv,
+    VideoDecoder,
+    VideoDecoderTrainer,
+)
 
-from brain2face.datasets import NeuroDiffusionCLIPEmbVideoDataset
+from brain2face.datasets import (
+    NeuroDiffusionCLIPEmbVideoDataset,
+    CollateFunctionForVideoHDF5,
+)
 
 
 def train() -> None:
@@ -38,6 +46,8 @@ def train() -> None:
 
     device = f"cuda:{args.cuda_id}"
 
+    logging.getLogger().setLevel(eval(f"logging.{args.log_level}"))
+
     # -----------------------
     #       Dataloader
     # -----------------------
@@ -46,10 +56,18 @@ def train() -> None:
 
     loader_args = {"drop_last": True, "num_workers": 4, "pin_memory": True}
     train_loader = torch.utils.data.DataLoader(
-        dataset=train_set, batch_size=args.batch_size, shuffle=True, **loader_args
+        dataset=train_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        collate_fn=CollateFunctionForVideoHDF5(train_set.Y_ref),
+        **loader_args,
     )
     test_loader = torch.utils.data.DataLoader(
-        dataset=test_set, batch_size=args.batch_size, shuffle=False, **loader_args
+        dataset=test_set,
+        batch_size=args.batch_size,
+        shuffle=False,
+        collate_fn=CollateFunctionForVideoHDF5(test_set.Y_ref),
+        **loader_args,
     )
 
     # ---------------------
@@ -76,6 +94,7 @@ def train() -> None:
         frame_sizes=tuple(args.frame_sizes),
         frame_numbers=tuple(args.frame_numbers),
         timesteps=args.timesteps,
+        learned_variance=False,
     ).to(device)
 
     # ---------------------
@@ -83,30 +102,14 @@ def train() -> None:
     # ---------------------
     decoder_trainer = VideoDecoderTrainer(
         decoder,
+        # accum_grad=args.accum_grad,
+        # sub_batch_size=args.sub_batch_size if args.accum_grad else None,
         lr=args.lr,
         wd=args.wd,
         ema_beta=args.ema_beta,
         ema_update_after_step=args.ema_update_after_step,
         ema_update_every=args.ema_update_every,
     )
-
-    # params = diffusion_prior.parameters()
-
-    # optimizer = torch.optim.Adam(params, lr=args.lr)
-
-    # if args.lr_scheduler == "cosine":
-    #     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-    #         optimizer, T_max=args.epochs, eta_min=args.lr * 0.01
-    #     )
-    # elif args.lr_scheduler == "multistep":
-    #     mlstns = [int(m * args.epochs) for m in args.lr_multistep_mlstns]
-    #     scheduler = torch.optim.lr_scheduler.MultiStepLR(
-    #         optimizer, milestones=mlstns, gamma=args.lr_step_gamma
-    #     )
-    # elif args.lr_scheduler == "none":
-    #     scheduler = None
-    # else:
-    #     raise ValueError
 
     # -----------------------
     #     Strat training
@@ -119,42 +122,27 @@ def train() -> None:
         test_losses_unet1 = []
         test_losses_unet2 = []
 
-        # diffusion_prior.train()
         for Y_embed, Y in tqdm(train_loader):
             Y_embed, Y = Y_embed.to(device), Y.to(device)
 
             loss_unet1 = decoder_trainer(video_embed=Y_embed, video=Y, unet_number=1)
-            # , max_batch_size=4)
             decoder_trainer.update(1)
 
             loss_unet2 = decoder_trainer(video_embed=Y_embed, video=Y, unet_number=2)
-            # , max_batch_size=4)
             decoder_trainer.update(2)
 
             train_losses_unet1.append(loss_unet1)
             train_losses_unet2.append(loss_unet2)
 
-            # optimizer.zero_grad()
-
-            # loss.backward()
-
-            # optimizer.step()
-
-        # diffusion_prior.eval()
-        for Y_embed, Y in test_loader:
+        for Y_embed, Y in tqdm(test_loader):
             Y_embed, Y = Y_embed.to(device), Y.to(device)
 
-            # with torch.no_grad():
             loss_unet1 = decoder_trainer(video_embed=Y_embed, video=Y, unet_number=1)
-            # , max_batch_size=4)
 
             loss_unet2 = decoder_trainer(video_embed=Y_embed, video=Y, unet_number=2)
 
             test_losses_unet1.append(loss_unet1)
             test_losses_unet2.append(loss_unet2)
-
-            # test_top10_accs.append(test_top10_acc)
-            # test_top1_accs.append(test_top1_acc)
 
         print(
             f"Epoch {epoch}/{args.epochs} | ",
@@ -162,7 +150,6 @@ def train() -> None:
             f"avg train loss unet2: {np.mean(train_losses_unet2):.3f} | ",
             f"avg test loss unet1: {np.mean(test_losses_unet1):.3f} | ",
             f"avg test loss unet2: {np.mean(test_losses_unet2):.3f} | ",
-            # f"lr: {optimizer.param_groups[0]['lr']:.5f}",
         )
 
         if args.use_wandb:
@@ -172,12 +159,10 @@ def train() -> None:
                 "train_loss_unet2": np.mean(train_losses_unet2),
                 "test_loss_unet1": np.mean(test_losses_unet1),
                 "test_loss_unet2": np.mean(test_losses_unet2),
-                # "lrate": optimizer.param_groups[0]["lr"],
+                "lrate_unet1": getattr(decoder_trainer, f"optim0").param_groups[0]["lr"],
+                "lrate_unet2": getattr(decoder_trainer, f"optim1").param_groups[0]["lr"],
             }
             wandb.log(performance_now)
-
-        # if scheduler is not None:
-        #     scheduler.step()
 
         torch.save(decoder.state_dict(), os.path.join(run_dir, "decoder_last.pt"))
 
