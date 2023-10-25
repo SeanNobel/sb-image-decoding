c14709f4df48c25dc25a1e90186a5d694d020cf6
SeanNobel <sensho19950503@gmail.com>
2023-10-25 14:33:35 +0900

    add count_parameters()

--- brain2face/utils/train_utils.py
+++ brain2face/utils/train_utils.py
@@ -9,6 +9,10 @@ from typing import Union, Optional, Callable, Dict
 from termcolor import cprint
 
 
+def count_parameters(model: nn.Module) -> int:
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
 class Models:
     def __init__(
         self,
