9c2c46919b704ebeab378a7290e3230e365614c8
SeanNobel <sensho19950503@gmail.com>
2023-11-27 15:05:24 +0900

    move splitting to Dataset from preprocessing

--- brain2face/datasets/things_meg.py
+++ brain2face/datasets/things_meg.py
@@ -1,41 +1,147 @@
 import os, sys
 import torch
+import numpy as np
 import mne
+from PIL import Image
 from termcolor import cprint
-from time import time
+from glob import glob
+from natsort import natsorted
+from typing import Tuple
+
+import clip
 
 
 class ThingsMEGCLIPDataset(torch.utils.data.Dataset):
-    def __init__(self, args, train: bool = True) -> None:
+    def __init__(self, args) -> None:
         super().__init__()
 
-        # Load preprocessed data in Hebart et al. (2023). There are slight differences from Meta paper.
-        # It looks that scaling and baseline correction is performed but clamping is not.
-        cprint("Loading epochs...", "cyan")
-        
+        categories = np.loadtxt(
+            os.path.join(args.metadata_dir, "Concept-specific/unique_id.csv"),
+            dtype=str,
+        )
+        category_idxs = np.loadtxt(
+            os.path.join(args.metadata_dir, "Concept-specific/image_concept_index.csv"),
+            dtype=int,
+        )
+        cprint(f"Categories: {categories.shape} | category indices: {category_idxs.shape}", "cyan")  # fmt: skip
+
         meg_paths = [
-            os.path.join(
-                args.data_root, f"derivatives/preprocessed/preprocessed_P{i+1}-epo.fif"
-            )
-            for i in range(1)
+            os.path.join(args.preprocessed_data_dir, f"MEG_P{i+1}.pt") for i in range(4)
         ]
-        epochs = [mne.read_epochs(path) for path in meg_paths]
+        sample_attrs_paths = [
+            os.path.join(args.thingsmeg_dir, f"sourcedata/sample_attributes_P{i+1}.csv")
+            for i in range(4)
+        ]
 
-        # Resample from 200Hz to 120Hz as in Meta paper.
-        cprint("Resampling epochs...", "cyan")
-        stime = time()
-        
-        epochs = [epoch.resample(args.brain_resample_sfreq, n_jobs=24) for epoch in epochs]
-        
-        cprint(f"Done. ({time() - stime:.2f}s)", "cyan")
+        X_list = []
+        self.y_list = []
+        subject_idxs_list = []
+        train_idxs_list = []
+        test_idxs_list = []
+        for subject_id, (meg_path, sample_attrs_path) in enumerate(
+            zip(meg_paths, sample_attrs_paths)
+        ):
+            # MEG
+            X_list.append(torch.load(meg_path))  # ( 27048, 271, segment_len )
 
-        X = [torch.from_numpy(epoch.get_data()) for epoch in epochs]
+            # Image (path) and subject index
+            sample_attrs = np.loadtxt(
+                sample_attrs_path, dtype=str, delimiter=",", skiprows=1
+            )  # ( 27048, 18 )
 
-        # Image ID in the dataset.
-        y = [torch.from_numpy(epoch.events[:, 2]) for epoch in epochs]
+            self.y_list += [
+                os.path.join(args.images_dir, "/".join(path.split("/")[1:]))
+                if not "images_test" in path
+                else os.path.join(
+                    args.images_dir,
+                    "_".join(os.path.basename(path).split("_")[:-1]),
+                    os.path.basename(path),
+                )
+                for path in sample_attrs[:, 8]
+            ]
 
-        subject_idxs = [torch.ones_like(y_) * i for i, y_ in enumerate(y)]
+            subject_idxs_list.append(
+                torch.ones(len(sample_attrs), dtype=int) * subject_id
+            )
 
-        print(X[0].shape, y[0].shape, subject_idxs[0].shape)
+            # Split
+            train_idxs, test_idxs = self.make_split(
+                sample_attrs, refined=args.refined_split
+            )
+            idx_offset = len(sample_attrs) * subject_id
+            train_idxs_list.append(train_idxs + idx_offset)
+            test_idxs_list.append(test_idxs + idx_offset)
 
-        sys.exit()
+        self.X = torch.cat(X_list, dim=0)
+        self.subject_idxs = torch.cat(subject_idxs_list, dim=0)
+
+        self.train_idxs = torch.cat(train_idxs_list, dim=0)
+        self.test_idxs = torch.cat(test_idxs_list, dim=0)
+
+        cprint(f"X: {self.X.shape} | y (paths): {len(self.y_list)} | subject_idxs: {self.subject_idxs.shape} | train_idxs: {self.train_idxs.shape} | test_idxs: {self.test_idxs.shape}", "cyan")  # fmt: skip
+
+        if args.vision.pretrained:
+            _, self.preprocess = clip.load(args.vision.pretrained_model)
+        else:
+            self.preprocess = None
+
+        self.subject_names = [f"s0{i+1}" for i in range(4)]
+
+    def __len__(self) -> int:
+        return len(self.y)
+
+    def __getitem__(self, i):
+        Y = Image.open(self.y_list[i]).convert("RGB")
+
+        if self.preprocess is not None:
+            Y = self.preprocess(Y)
+        else:
+            raise NotImplementedError
+
+        return self.X[i], Y, self.subject_idxs[i]
+
+    @staticmethod
+    def make_split(
+        sample_attrs: np.ndarray,
+        refined: bool = True,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Args:
+            sample_attrs ( 27048, 18 ): Elements are strs.
+            refined (bool): If True, use splits by Meta, modified from Hebart et al., 2023.
+        Returns:
+            train_trial_idxs ( 22248, ): _description_
+            test_trial_idxs ( 2400, ): _description_
+        """
+        trial_types = sample_attrs[:, 0]  # ( 27048, )
+
+        if not refined:
+            train_trial_idxs = np.where(trial_types == "exp")[0]  # ( 22248, )
+            test_trial_idxs = np.where(trial_types == "test")[0]  # ( 2400, )
+
+        else:
+            category_idxs = sample_attrs[:, 2].astype(int)  # ( 27048, )
+
+            test_trial_idxs = np.where(trial_types == "test")[0]  # ( 2400, )
+            test_category_idxs = np.unique(np.take(category_idxs, test_trial_idxs))
+            # ( 200, )
+
+            test_trial_idxs = np.where(np.isin(category_idxs, test_category_idxs))[0]
+
+            train_trial_idxs = np.where(
+                np.logical_and(
+                    trial_types == "exp",
+                    np.logical_not(np.isin(category_idxs, test_category_idxs)),
+                )
+            )[0]
+
+        return torch.from_numpy(train_trial_idxs), torch.from_numpy(test_trial_idxs)
+
+
+if __name__ == "__main__":
+    from hydra import initialize, compose
+
+    with initialize(version_base=None, config_path="../../configs/thingsmeg"):
+        args = compose(config_name="clip")
+
+    dataset = ThingsMEGCLIPDataset(args)
--- brain2face/preprocs/things_meg.py
+++ brain2face/preprocs/things_meg.py
@@ -5,18 +5,16 @@ to 120Hz as in the Meta paper.
 """
 import os, sys
 import numpy as np
-import torch
 import mne
+import torch
 from termcolor import cprint
 from natsort import natsorted
 import hydra
 from omegaconf import DictConfig
 
 
-def modified_split(epochs: mne.Epochs, category_idxs: np.ndarray):
-    """
-    Splits by Meta, modified from Hebart et al., 2023.
-    """
+def make_refined_split_from_file(epochs: mne.Epochs, category_idxs: np.ndarray):
+    """Old version of make_split."""
     # NOTE: Event ids in the preprocessed data seem to start from 1.
     trials = epochs.events[:, -1] - 1  # ( 27048, )
 
@@ -48,76 +46,27 @@ def modified_split(epochs: mne.Epochs, category_idxs: np.ndarray):
     version_base=None, config_path="../../configs/thingsmeg", config_name="clip"
 )
 def run(args: DictConfig) -> None:
-    categories = natsorted(os.listdir(args.images_dir))
-    categories = [
-        c for c in categories if os.path.isdir(os.path.join(args.images_dir, c))
-    ]  # ( 1854, )
-    cprint(f"Found {len(categories)} categories.", "cyan")
-
-    num_imgs_per_cat = [
-        len(os.listdir(os.path.join(args.images_dir, c))) for c in categories
-    ]  # ( 1854, )
-    category_idxs = np.repeat(
-        np.arange(len(categories)), num_imgs_per_cat
-    )  # ( 26107, )
-    cprint(f"Total number of images: {len(category_idxs)}", "cyan")
-
     meg_paths = [
         os.path.join(args.meg_dir, f"preprocessed_P{i+1}-epo.fif") for i in range(4)
     ]
 
-    train_X_list = []
-    train_y_list = []
-    train_subject_idxs_list = []
-    test_X_list = []
-    test_y_list = []
-    test_subject_idxs_list = []
     for subject_id, meg_path in enumerate(meg_paths):
         cprint(f"==== Processing subject {subject_id+1} ====", "cyan")
+
         cprint("> Loading epochs...", "cyan")
         epochs = mne.read_epochs(meg_path)
-
-        train_trial_idxs, test_trial_idxs = modified_split(epochs, category_idxs)
 
         cprint(f"> Resampling epochs to {args.brain_resample_sfreq}Hz...", "cyan")
         epochs = epochs.resample(args.brain_resample_sfreq, n_jobs=8)
 
-        X = epochs.get_data()  # ( 27048, 271, segment_len )
-        y = epochs.events[:, -1]  # ( 27048, )
-        subject_idxs = np.ones_like(y) * subject_id  # ( 27048, )
+        X = torch.from_numpy(epochs.get_data()).to(torch.float32)
+        # ( 27048, 271, segment_len )
 
-        train_X_list.append(X[train_trial_idxs])
-        train_y_list.append(y[train_trial_idxs])
-        train_subject_idxs_list.append(subject_idxs[train_trial_idxs])
-        test_X_list.append(X[test_trial_idxs])
-        test_y_list.append(y[test_trial_idxs])
-        test_subject_idxs_list.append(subject_idxs[test_trial_idxs])
+        cprint(f"MEG P{subject_id+1}: {X.shape}", "cyan")
 
-    train_X = np.concatenate(train_X_list, axis=0)
-    train_y = np.concatenate(train_y_list, axis=0)
-    train_subject_idxs = np.concatenate(train_subject_idxs_list, axis=0)
-    test_X = np.concatenate(test_X_list, axis=0)
-    test_y = np.concatenate(test_y_list, axis=0)
-    test_subject_idxs = np.concatenate(test_subject_idxs_list, axis=0)
-
-    cprint(
-        f"[Train] X: {train_X.shape}, y: {train_y.shape}, subject_idxs: {train_subject_idxs.shape}",
-        "cyan",
-    )
-    cprint(
-        f"[Test] X: {test_X.shape}, y: {test_y.shape}, subject_idxs: {test_subject_idxs.shape}",
-        "cyan",
-    )
-
-    np.savez(
-        os.path.join(args.preprocessed_data_dir, args.preproc_name),
-        train_X=train_X,
-        train_y=train_y,
-        train_subject_idxs=train_subject_idxs,
-        test_X=test_X,
-        test_y=test_y,
-        test_subject_idxs=test_subject_idxs,
-    )
+        torch.save(
+            X, os.path.join(args.preprocessed_data_dir, f"MEG_P{subject_id+1}.pt")
+        )
 
 
 if __name__ == "__main__":
