ce3893c25022dddc4bd31b97f0a8e350ce51f71c
SeanNobel <sensho19950503@gmail.com>
2023-12-07 17:51:00 +0900

    minor updates

--- brain2face/utils/plots.py
+++ brain2face/utils/plots.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
+
 def plot_latents_2d(
     latents: np.ndarray,
     classes: np.ndarray,
@@ -15,20 +16,21 @@ def plot_latents_2d(
     latents ( samples, dim )
     classes ( samples, )
     """
-    classes /= classes.max()
-        
+    classes = classes.astype(float) / classes.max()
+
     fig, axs = plt.subplots(ncols=5, figsize=(20, 5), tight_layout=True)
     fig.suptitle(f"Originally {latents.shape[1]} dimensions")
-    
+
     latents_reduced = PCA(n_components=2).fit_transform(latents)
     axs[0].scatter(*latents_reduced.T, c=classes, cmap=cmap)
     axs[0].set_title("PCA")
-        
+
     for perplexity, ax in zip([2, 10, 40, 100], axs[1:]):
-        latents_reduced = TSNE(n_components=2, perplexity=perplexity).fit_transform(latents)
+        latents_reduced = TSNE(n_components=2, perplexity=perplexity).fit_transform(latents)  # fmt: skip
+
         ax.scatter(*latents_reduced.T, c=classes, cmap=cmap)
         ax.set_title(f"t-SNE (perplexity={perplexity})")
-        
+
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     fig.savefig(os.path.join(save_dir, f"epoch{epoch}.png"))
