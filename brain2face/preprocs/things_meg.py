7bf71af3e266f28834be2032254827ba72de39d6
SeanNobel <sensho19950503@gmail.com>
2023-12-11 15:29:44 +0900

    move images encoding from dataset to preprocessing

--- brain2face/preprocs/things_meg.py
+++ brain2face/preprocs/things_meg.py
@@ -7,10 +7,38 @@ import os, sys
 import numpy as np
 import mne
 import torch
+import clip
+from PIL import Image
+from functools import partial
 from termcolor import cprint
 from natsort import natsorted
+from tqdm import tqdm
+from typing import Tuple, List
 import hydra
 from omegaconf import DictConfig
+
+from brain2face.utils.brain_preproc import scale_clamp
+from brain2face.utils.train_utils import sequential_apply
+
+
+@torch.no_grad()
+def encode_images(y_list: List[str], preprocess, clip_model, device) -> torch.Tensor:
+    Y = torch.stack(
+        [
+            preprocess(Image.open(y).convert("RGB"))
+            for y in tqdm(y_list, desc="Preprocessing images")
+        ]
+    )
+
+    Y = sequential_apply(
+        Y,
+        clip_model.encode_image,
+        batch_size=32,
+        device=device,
+        desc="Encoding images",
+    ).float()
+
+    return Y
 
 
 def make_refined_split_from_file(epochs: mne.Epochs, category_idxs: np.ndarray):
@@ -49,24 +77,80 @@ def run(args: DictConfig) -> None:
     meg_paths = [
         os.path.join(args.meg_dir, f"preprocessed_P{i+1}-epo.fif") for i in range(4)
     ]
+    sample_attrs_paths = [
+        os.path.join(args.thingsmeg_dir, f"sourcedata/sample_attributes_P{i+1}.csv")
+        for i in range(4)
+    ]
 
-    for subject_id, meg_path in enumerate(meg_paths):
+    save_dir = os.path.join(args.preprocessed_data_dir, args.preproc_name)
+    os.makedirs(save_dir, exist_ok=True)
+
+    for subject_id, (meg_path, sample_attrs_path) in enumerate(zip(meg_paths, sample_attrs_paths)):  # fmt: skip
         cprint(f"==== Processing subject {subject_id+1} ====", "cyan")
 
-        cprint("> Loading epochs...", "cyan")
-        epochs = mne.read_epochs(meg_path)
+        # -----------------
+        #        MEG
+        # -----------------
+        if not args.skip_meg:
+            cprint("> Loading epochs...", "cyan")
+            epochs = mne.read_epochs(meg_path)
 
-        cprint(f"> Resampling epochs to {args.brain_resample_sfreq}Hz...", "cyan")
-        epochs = epochs.resample(args.brain_resample_sfreq, n_jobs=8)
+            cprint(f"> Resampling epochs to {args.brain_resample_sfreq}Hz...", "cyan")
+            epochs.resample(args.brain_resample_sfreq, n_jobs=8)
 
-        X = torch.from_numpy(epochs.get_data()).to(torch.float32)
-        # ( 27048, 271, segment_len )
+            cprint(f"> Scale and clamping epochs to ±{args.clamp_lim}...", "cyan")
+            epochs.apply_function(
+                partial(scale_clamp, scale_transposed=False, clamp_lim=args.clamp_lim),
+                n_jobs=8,
+            )
 
-        cprint(f"MEG P{subject_id+1}: {X.shape}", "cyan")
+            cprint("> Baseline correction...", "cyan")
+            epochs.apply_baseline((None, 0))
 
-        torch.save(
-            X, os.path.join(args.preprocessed_data_dir, f"MEG_P{subject_id+1}.pt")
-        )
+            X = torch.from_numpy(epochs.get_data()).to(torch.float32)
+            # ( 27048, 271, segment_len )
+
+            cprint(f"MEG P{subject_id+1}: {X.shape}", "cyan")
+
+            torch.save(X, os.path.join(save_dir, f"MEG_P{subject_id+1}.pt"))
+
+        # -----------------
+        #      Images
+        # -----------------
+        if args.vision.pretrained and not args.skip_images:
+            device = f"cuda:{args.cuda_id}"
+
+            clip_model, preprocess = clip.load(args.vision.pretrained_model)
+            clip_model = clip_model.eval().to(device)
+
+            sample_attrs = np.loadtxt(
+                sample_attrs_path, dtype=str, delimiter=",", skiprows=1
+            )
+
+            y_list = []
+            for path in sample_attrs[:, 8]:
+                if "images_meg" in path:
+                    y_list.append(
+                        os.path.join(args.images_dir, "/".join(path.split("/")[1:]))
+                    )
+                elif "images_test_meg" in path:
+                    y_list.append(
+                        os.path.join(
+                            args.images_dir,
+                            "_".join(os.path.basename(path).split("_")[:-1]),
+                            os.path.basename(path),
+                        )
+                    )
+                elif "images_catch_meg" in path:
+                    y_list.append(os.path.join(args.images_dir, "black.jpg"))
+                else:
+                    raise ValueError(f"Unknown image path type: {path}")
+
+            Y = encode_images(y_list, preprocess, clip_model, device)
+
+            cprint(f"Images P{subject_id+1}: {Y.shape}", "cyan")
+
+            torch.save(Y, os.path.join(save_dir, f"Images_P{subject_id+1}.pt"))
 
 
 if __name__ == "__main__":
