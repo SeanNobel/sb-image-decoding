13d47d0f120abec04d9e3cca13bea2852c6f386f
SeanNobel <sensho19950503@gmail.com>
2023-12-19 19:26:01 +0900

    27 higher categories for plotting latents

--- brain2face/datasets/things_meg.py
+++ brain2face/datasets/things_meg.py
@@ -18,15 +18,25 @@ class ThingsMEGCLIPDataset(torch.utils.data.Dataset):
 
         self.large_test_set = args.large_test_set
 
-        categories = np.loadtxt(
-            os.path.join(args.metadata_dir, "Concept-specific/unique_id.csv"),
-            dtype=str,
-        )
-        category_idxs = np.loadtxt(
-            os.path.join(args.metadata_dir, "Concept-specific/image_concept_index.csv"),
+        # categories = np.loadtxt(
+        #     os.path.join(args.metadata_dir, "Concept-specific/unique_id.csv"),
+        #     dtype=str,
+        # )
+        # category_idxs = np.loadtxt(
+        #     os.path.join(args.metadata_dir, "Concept-specific/image_concept_index.csv"),
+        #     dtype=int,
+        # )
+        # cprint(f"Categories: {categories.shape} | category indices: {category_idxs.shape}", "cyan")  # fmt: skip
+
+        # NOTE: Some categories
+        high_categories = np.loadtxt(
+            os.path.join(
+                args.things_dir, "27 higher-level categories/category_mat_manual.tsv"
+            ),
             dtype=int,
-        )
-        cprint(f"Categories: {categories.shape} | category indices: {category_idxs.shape}", "cyan")  # fmt: skip
+            delimiter="\t",
+            skiprows=1,
+        )  # ( 1854, 27 )
 
         preproc_dir = os.path.join(args.preprocessed_data_dir, args.preproc_name)
 
@@ -78,8 +88,14 @@ class ThingsMEGCLIPDataset(torch.utils.data.Dataset):
         self.Y = torch.cat(Y_list, dim=0)
         self.subject_idxs = torch.cat(subject_idxs_list, dim=0)
 
-        self.categories = torch.cat(categories_list)
-        self.y_idxs = torch.cat(y_idxs_list)
+        self.categories = torch.cat(categories_list) - 1
+        assert torch.equal(self.categories.unique(), torch.arange(self.categories.max() + 1))  # fmt: skip
+        self.num_categories = len(self.categories.unique())
+
+        self.high_categories = self.to_high_categories(self.categories, high_categories)
+
+        self.y_idxs = torch.cat(y_idxs_list) - 1
+        assert torch.equal(self.y_idxs.unique(), torch.arange(self.y_idxs.max() + 1))
 
         self.train_idxs = torch.cat(train_idxs_list, dim=0)
         self.test_idxs = torch.cat(test_idxs_list, dim=0)
@@ -98,7 +114,7 @@ class ThingsMEGCLIPDataset(torch.utils.data.Dataset):
         return len(self.y)
 
     def __getitem__(self, i):
-        return self.X[i], self.Y[i], self.subject_idxs[i], self.categories[i], self.y_idxs[i]  # fmt: skip
+        return self.X[i], self.Y[i], self.subject_idxs[i], self.y_idxs[i], self.categories[i], self.high_categories[i]  # fmt: skip
 
     @staticmethod
     def make_split(
@@ -147,6 +163,28 @@ class ThingsMEGCLIPDataset(torch.utils.data.Dataset):
             assert len(train_trial_idxs) == 19848 and len(test_trial_idxs) == 2400
 
         return torch.from_numpy(train_trial_idxs), torch.from_numpy(test_trial_idxs)
+
+    def to_high_categories(
+        self, categories: torch.Tensor, high_categories: np.ndarray
+    ) -> torch.Tensor:
+        """_summary_
+        Args:
+            categories ( 27048 * 4, ): Elements are integers of [0, 1854].
+                End value 1854 is for catch trials.
+            high_categories ( 1854, 27 ): Each row is a zero to three -hot vector.
+        Returns:
+            high_categories ( 27048 * 4, ): Elements are integers of [0, 27].
+        """
+        # Set categories that are not in any of higher categories as "uncategorized".
+        unc = np.where(high_categories.sum(axis=1) == 0)[0]
+        # This takes the first higher-category for categories that are in multiple higher-categories.
+        high_categories = np.argmax(high_categories, axis=1)  # ( 1854, )
+
+        # Higher-category 27 for "uncategorized" and catch trials.
+        high_categories[unc] = high_categories.max() + 1  # ( 1854, )
+        high_categories = np.append(high_categories, high_categories.max())  # ( 1855, )
+
+        return torch.from_numpy(high_categories)[categories]
 
 
 if __name__ == "__main__":
