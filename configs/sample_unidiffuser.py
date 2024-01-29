c8ee901df9f48a5dfec63273bf1ab3b09610603c
SeanNobel <sensho19950503@gmail.com>
2024-01-29 17:47:34 +0900

    :construction: matching model configurations for training and sampling

--- configs/sample_unidiffuser.py
+++ configs/sample_unidiffuser.py
@@ -12,13 +12,13 @@ def get_config():
     config.seed = 1234
     config.pred = "noise_pred"
     config.z_shape = (4, 64, 64)  # ( c' , h' , w' )
-    config.clip_img_dim = 512
+    config.clip_img_dim = 768  # 512
     config.clip_text_dim = 768
-    config.text_dim = 64  # reduce dimension
+    config.text_dim = 768  # 64  # reduce dimension
     config.data_type = 1
 
     config.autoencoder = d(
-        pretrained_path="models/autoencoder_kl.pth",
+        pretrained_path="U-ViT/assets/stable-diffusion/autoencoder_kl.pth",
     )
 
     config.caption_decoder = d(
@@ -28,12 +28,12 @@ def get_config():
 
     config.nnet = d(
         name="uvit_multi_post_ln_v1",
-        img_size=64,
+        img_size=32,  # 64,
         in_chans=4,
         patch_size=2,
-        embed_dim=1536,
-        depth=30,
-        num_heads=24,
+        embed_dim=512,  # 1536,
+        depth=12,  # 30,
+        num_heads=8,  # 24,
         mlp_ratio=4,
         qkv_bias=False,
         pos_drop_rate=0.0,
@@ -41,11 +41,11 @@ def get_config():
         attn_drop_rate=0.0,
         mlp_time_embed=False,
         text_dim=config.get_ref("text_dim"),
-        num_text_tokens=77,
+        num_text_tokens=1,  # 77,
         clip_img_dim=config.get_ref("clip_img_dim"),
         use_checkpoint=True,
     )
 
-    config.sample = d(sample_steps=50, scale=7.0, t2i_cfg_mode="true_uncond")
+    config.sample = d(sample_steps=50, scale=7.0, b2i_cfg_mode="true_uncond")
 
     return config
