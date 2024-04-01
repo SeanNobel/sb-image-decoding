92ebcee6a4327664a522077cfbad6231cd945108
SeanNobel <sensho19950503@gmail.com>
2024-04-01 20:20:23 +0900

    multiple noise patterns (resembling multiple subjects in MEG)

--- nd/datasets/things_text.py
+++ nd/datasets/things_text.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 import csv
 from termcolor import cprint
-from typing import List
 
 import clip
 
@@ -15,6 +14,7 @@ class ThingsTextCLIPDataset(ThingsCLIPDatasetBase):
         super().__init__()
 
         self.num_clip_tokens = args.num_clip_tokens
+        num_noises = args.num_noises
 
         self.preproc_dir = os.path.join(args.preprocessed_data_dir, args.preproc_name)
 
@@ -25,6 +25,8 @@ class ThingsTextCLIPDataset(ThingsCLIPDatasetBase):
         sample_attrs = np.loadtxt(
             sample_attrs_path, dtype=str, delimiter=",", skiprows=1
         )  # ( 27048, 18 )
+
+        self.num_samples = len(sample_attrs)
 
         # -------------
         #    Images
@@ -37,17 +39,21 @@ class ThingsTextCLIPDataset(ThingsCLIPDatasetBase):
         # -------------
         #     Text
         # -------------
-        text_path = os.path.join(self.preproc_dir, "Captions.csv")
-        text_dict = {key: text for key, text in csv.reader(open(text_path, "r"))}
         keys = [os.path.splitext(os.path.basename(key))[0] for key in sample_attrs[:, 8]]  # fmt: skip
-        texts = [text_dict[key] if key in text_dict else "" for key in keys]
 
-        if args.noise_level > 0:
-            texts = self._add_noise(texts, args.noise_level)
+        texts = []
+        for n in range(num_noises):
+            texts_path = os.path.join(
+                self.preproc_dir, f"noise_level_{args.noise_level}", f"Texts_N{n}.csv"
+            )
+            texts_dict = {key: text for key, text in csv.reader(open(texts_path, "r"))}
+            texts += [texts_dict[key] if key in texts_dict else "" for key in keys]
 
-        self.X = clip.tokenize(texts)  # ( 27048, 77 )
-        self.num_samples = len(self.X)
+        self.X = clip.tokenize(texts)  # ( 27048 * num_noises, 77 )
 
+        # -------------
+        #    Others
+        # -------------
         self.categories = torch.from_numpy(sample_attrs[:, 2].astype(int))
         self.num_categories = len(self.categories.unique())
 
@@ -57,33 +63,23 @@ class ThingsTextCLIPDataset(ThingsCLIPDatasetBase):
             sample_attrs, large_test_set=args.large_test_set
         )
 
-        Y_shape = self.Y.shape if hasattr(self, "Y") else "to be loaded in __getitem__"
-        cprint(f"X: {self.X.shape} | Y: {Y_shape} | train_idxs: {self.train_idxs.shape} | test_idxs: {self.test_idxs.shape}", "cyan")  # fmt: skip
+        # ----------------------------------
+        # Repeat for number of noisy samples
+        # ----------------------------------
+        self.Y = self.Y.repeat(num_noises, 1, 1)  # ( 27048 * num_noises, 1, 768 )
+        self.categories = self.categories.repeat(num_noises)
+        self.y_idxs = self.y_idxs.repeat(num_noises)
+        self.train_idxs = torch.cat(
+            [self.train_idxs + i * self.num_samples for i in range(num_noises)]
+        )
+        self.test_idxs = torch.cat(
+            [self.test_idxs + i * self.num_samples for i in range(num_noises)]
+        )
+
+        cprint(f"X: {self.X.shape} | Y: {self.Y.shape} | train_idxs: {self.train_idxs.shape} | test_idxs: {self.test_idxs.shape}", "cyan")  # fmt: skip
 
     def __len__(self) -> int:
-        return self.num_samples
+        return len(self.X)
 
     def __getitem__(self, i: int):
         return self.X[i], self.Y[i], self.y_idxs[i], self.categories[i]
-
-    @staticmethod
-    def _add_noise(texts: List[str], noise_level: float) -> List[str]:
-        """Randomly replace characters in text with noise_level probability."""
-        noisy_texts = []
-        for i, text in enumerate(texts):
-            noisy_text = ""
-
-            for char in text:
-                if np.random.rand() < noise_level:
-                    noisy_text += chr(np.random.randint(32, 127))
-                else:
-                    noisy_text += char
-
-            noisy_texts.append(noisy_text)
-
-            # cprint(text, "cyan")
-            # cprint(noisy_text, "yellow")
-            # if i > 10:
-            #     sys.exit()
-
-        return noisy_texts
