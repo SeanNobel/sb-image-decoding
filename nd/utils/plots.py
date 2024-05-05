3b40ec2fe14f12dc98e1c9e4aca8cce5d785f755
SeanNobel <sensho19950503@gmail.com>
2024-05-05 03:00:36 +0000

    set PCA to option, optionally off axis

--- nd/utils/plots.py
+++ nd/utils/plots.py
@@ -4,7 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-from typing import Optional
+from typing import Optional, List
 from termcolor import cprint
 
 matplotlib.use("Agg")
@@ -13,8 +13,11 @@ matplotlib.use("Agg")
 def plot_latents_2d(
     latents: np.ndarray,
     classes: np.ndarray,
+    perplexities: List[int] = [2, 10, 40, 100],
+    pca: bool = True,
     save_path: Optional[str] = None,
     cmap: str = "gist_rainbow",
+    off_axis: bool = False,
 ) -> Optional[plt.Figure]:
     """
     latents ( samples, dim )
@@ -26,19 +29,32 @@ def plot_latents_2d(
 
     classes = classes.astype(float) / classes.max()
 
-    fig, axs = plt.subplots(ncols=5, figsize=(20, 5), tight_layout=True)
-    fig.suptitle(f"Originally {latents.shape[1]} dimensions")
+    ncols = len(perplexities) + pca
+    fig, axs = plt.subplots(ncols=ncols, figsize=(ncols * 4, 5), tight_layout=True, squeeze=False)
+    # NOTE: removing row axis that's always 1
+    axs = axs[0]
 
-    latents_reduced = PCA(n_components=2).fit_transform(latents)
-    axs[0].scatter(*latents_reduced.T, c=classes, cmap=cmap)
-    axs[0].set_title("PCA")
+    if not off_axis:
+        fig.suptitle(f"Originally {latents.shape[1]} dimensions")
 
-    for perplexity, ax in zip([2, 10, 40, 100], axs[1:]):
+    if pca:
+        latents_reduced = PCA(n_components=2).fit_transform(latents)
+        axs[0].scatter(*latents_reduced.T, c=classes, cmap=cmap)
+        if off_axis:
+            axs[0].axis("off")
+        else:
+            axs[0].set_title("PCA")
+
+    axs_tsne = axs[1:] if pca else axs
+    for perplexity, ax in zip(perplexities, axs_tsne):
         print(f"Running t-SNE with perplexity={perplexity}...")
         latents_reduced = TSNE(n_components=2, perplexity=perplexity).fit_transform(latents)  # fmt: skip
 
-        ax.scatter(*latents_reduced.T, c=classes, cmap=cmap)
-        ax.set_title(f"t-SNE (perplexity={perplexity})")
+        ax.scatter(*latents_reduced.T, c=classes, cmap=cmap, s=2)
+        if off_axis:
+            ax.axis("off")
+        else:
+            ax.set_title(f"t-SNE (perplexity={perplexity})")
 
     if save_path is not None:
         if not os.path.exists(os.path.dirname(save_path)):
@@ -53,7 +69,9 @@ def plot_2d_latents_with_sorted_categories(
 ):
     sorted_high_categories = ["bird", "insect", "plant", "body part", "animal", "vegetable", "fruit", "drink", "dessert", "food", "clothing", "clothing accessory", "furniture", "home decor", "toy", "kitchen appliance", "kitchen tool", "office supply", "sports equipment", "medical equipment", "tool", "musical instrument", "electronic device", "weapon", "vehicle", "part of car", "container"]  # fmt: skip
 
-    hc_path = "/mnt/tsukuyomi/things/osfstorage/THINGS/27 higher-level categories/category_mat_manual.tsv"
+    hc_path = (
+        "/mnt/tsukuyomi/things/osfstorage/THINGS/27 higher-level categories/category_mat_manual.tsv"
+    )
     high_categories = np.loadtxt(hc_path, dtype=int, delimiter="\t", skiprows=1)
     # ( 1854, 27 )
 
@@ -80,21 +98,13 @@ def plot_2d_latents_with_sorted_categories(
         return np.array([argsort[str(i)] for i in categories])
 
     fig, ax = plt.subplots(1, 4, figsize=(20, 5), tight_layout=True)
-    ax[0].scatter(
-        *train_Y.T, c=sort_categories(train_categories), s=1, cmap="gist_rainbow"
-    )
+    ax[0].scatter(*train_Y.T, c=sort_categories(train_categories), s=1, cmap="gist_rainbow")
     ax[0].set_title("Image train")
-    ax[1].scatter(
-        *test_Y.T, c=sort_categories(test_categories), s=1, cmap="gist_rainbow"
-    )
+    ax[1].scatter(*test_Y.T, c=sort_categories(test_categories), s=1, cmap="gist_rainbow")
     ax[1].set_title("Image test")
-    ax[2].scatter(
-        *train_Z.T, c=sort_categories(train_categories), s=1, cmap="gist_rainbow"
-    )
+    ax[2].scatter(*train_Z.T, c=sort_categories(train_categories), s=1, cmap="gist_rainbow")
     ax[2].set_title("Text (MEG) train")
-    ax[3].scatter(
-        *test_Z.T, c=sort_categories(test_categories), s=1, cmap="gist_rainbow"
-    )
+    ax[3].scatter(*test_Z.T, c=sort_categories(test_categories), s=1, cmap="gist_rainbow")
     ax[3].set_title("Text (MEG) test")
 
     fig.canvas.draw()
